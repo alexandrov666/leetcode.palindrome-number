b9bb1e656ce1550c8fd53c8ce4ef04c55a64d922
Александр <i.Alexandrov6662gmail.com>
2023-09-20 22:11:25 +0300

    Given an integer x, return true if x is a palindrome , and false otherwise.
    Example 1:
    
    Input: x = 121
    Output: true
    Explanation: 121 reads as 121 from left to right and from right to left.
    Example 2:
    
    Input: x = -121
    Output: false
    Explanation: From left to right, it reads -121. From right to left, it becomes 121-. Therefore it is not a palindrome.
    Example 3:
    
    Input: x = 10
    Output: false
    Explanation: Reads 01 from right to left. Therefore it is not a palindrome.
    
    Constraints:
    
    -231 <= x <= 231 - 1
    
    Follow up: Could you solve it without converting the integer to a string?
    
    Решил без строк, но по оптимизации вопрос открытый :)

--- Program.cs
+++ Program.cs
@@ -8,8 +8,40 @@ namespace leetcode.palindrome_number
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static int Main(string[] args)
         {
+            int x = 12312312;
+            if (x > 0)
+            {
+                int length = (int)Math.Floor(Math.Log10((double)x)) + 1;
+                int temp_left = (int)Math.Pow(10, length - 1);
+                int temp_right = 1;
+
+                for (int i = 0; i < length / 2; i++)
+                {
+                    if ((x / temp_left) % 10 != (x / temp_right) % 10)
+                    {
+                        return 0;
+                    }
+                    else
+                    {
+                        temp_left /= 10;
+                        temp_right *= 10;
+                    }
+                }
+                return 1;
+            }
+            else
+            {
+                if (x == 0)
+                {
+                    return 1;
+                }
+                else
+                {
+                    return 0;
+                }
+            }
         }
     }
 }
